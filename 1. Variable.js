f7d0af4a7e458aa8ef821ec5121beca3ce74103e
muhamadrizkiariffadillah <kilisarisol@gmail.com>
2024-07-14 10:06:00 +0700

    Day1: Learning variable and data types

---
+++ 1. Variable.js
@@ -0,0 +1,11 @@
+// Let for local variable, var for global variable
+let FullName = "Muhamad Rizki Arif Fadillah"
+let MyAge = 25
+let MyTal = 167.5
+let IsHandsome = true
+console.log(FullName, MyAge, MyTal, IsHandsome)
+
+// Constanta
+
+const IdUser = 1
+console.log(IdUser)
---
+++ 2. Data Types.js
@@ -0,0 +1,25 @@
+
+//Number
+let integer = 10
+let float = 10.5
+
+// string
+let text = "This is text"
+
+// boolean
+let bool = true
+let bool2 = false
+
+// object
+let user = {
+    firstName : "Muhamad Rizki",
+    lastName: "Arif Fadillah",
+    age: 25,
+}
+
+// Date object.
+
+let date = new Date("2024-07-31");
+
+console.log(integer,float,text,bool,bool2);
+console.log(user,date);
