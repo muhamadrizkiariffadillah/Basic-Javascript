dcf917d3b37b638debb3407b872ad67cec62ef53
muhamadrizkiariffadillah <kilisarisol@gmail.com>
2024-07-17 09:21:03 +0700

    Day 2: Learning Operators

---
+++ 3. Operators.js
@@ -0,0 +1,57 @@
+// Arithmetic Operators
+let x = 20;
+let y = 3;
+
+// Addition
+console.log(x + y);
+// Subtraction
+console.log(x - y);
+// Division
+console.log(x / y);
+// Multiplication
+console.log(x * y);
+// Exponentiation
+console.log(x ** y);
+// Modulo
+console.log(x % y);
+// Increment
+x++;
+console.log(x);
+// Decrement
+y--;
+console.log(y);
+
+// Assignment Operator
+// like arithmetic just different symbol
+console.log((x += y));
+console.log((x -= y));
+console.log((x *= y));
+console.log((x /= y));
+console.log((x **= y));
+console.log((x /= y));
+
+// Comparison Operators
+let a = 10;
+let b = 11;
+
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+
+// Logical Operators
+let benar = true;
+let salah = false;
+
+console.log(benar && benar);
+console.log(benar && salah);
+console.log(salah && benar);
+
+console.log(benar || benar);
+console.log(benar || salah);
+console.log(salah || benar);
+
+console.log(!benar);
+console.log(!salah);
